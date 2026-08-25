c18d723f06408f166bbe3a841213b9b9642dea14
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-25 14:28:28 +0800

    Added Arithmetic Operators

---
+++ src/arithmetic.java
@@ -0,0 +1,42 @@
+public class arithmetic {
+
+    public static void main(String[] args) {
+
+        // Arithmetic Operators
+
+//        int x = 10;
+//        int y = 2;
+//        int z;
+
+        //z = x + y;
+        //z = x - y;
+        //z = x * y;
+        //z = 10 / y;
+        //z = x % y;
+
+        // Augmented Assignment Operators
+
+        //int x = 10;
+        //int y = 3;
+
+        //x += y;
+        //x -= y;
+        //x *= y;
+        //x /= y;
+        //x %= y;
+
+
+        // Increment and Decrement Operators
+
+        //int x = 1;
+
+        //x++;
+        //x--;
+
+        // Order of Operations (P-E-M-D-A-S)
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+
+
+    }
+}
