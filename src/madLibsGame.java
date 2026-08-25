e49f8e95b9192c1fe84b5ab40604d25e77d20a53
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-25 12:25:08 +0800

    Created a user Input exercise Mad Libs(Not Finished)

---
+++ src/madLibsGame.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class madLibsGame {
+
+    public static void main(String[] args) {
+
+            // Mad Libs Game
+
+        Scanner scanner = new Scanner(System.in);
+
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+    }
+}
