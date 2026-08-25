ec7f914e5cb5b85bd045405b6b72721b98c8de8a
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-25 14:16:07 +0800

    Finished Mad libs

--- src/madLibsGame.java
+++ src/madLibsGame.java
@@ -14,11 +14,22 @@ public class madLibsGame {
         String verb1;
         String adjective3;
 
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.print("Enter an adjective (description): ");
+        adjective1 = scanner.nextLine();
+        System.out.print("Enter a noun (animal or person): ");
+        noun1 = scanner.nextLine();
+        System.out.print("Enter an adjective (description): ");
+        adjective2 = scanner.nextLine();
+        System.out.print("Enter a verb end with -ing (action): ");
+        verb1 = scanner.nextLine();
+        System.out.print("Enter an adjective (description): ");
+        adjective3 = scanner.nextLine();
 
+        System.out.println("\nToday I want to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "!");
 
+        scanner.close();
     }
 }
