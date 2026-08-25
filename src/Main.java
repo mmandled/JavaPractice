176ab630186bb601798e4d3c8c74b31c205d97f2
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-25 11:23:57 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main  {
+
+    public static void main(String[] args){
+
+        //This is my first JAVA program
+
+        System.out.println("I love pizza");
+        System.out.println("Its really good");
+        System.out.println("Buy me Pizza please");
+    }
+}
