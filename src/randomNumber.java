e32ace371b12420b04f4bcd829efc9bcf3fc3aca
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-26 08:06:53 +0800

    Added Random Numbers

---
+++ src/randomNumber.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class randomNumber {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int num1;
+        int num2;
+        int num3;
+
+        double num4;
+
+        boolean isheads;
+
+        num1 = random.nextInt(1, 101);
+        num2 = random.nextInt(1, 101);
+        num3 = random.nextInt(1, 101);
+
+        num4 = random.nextDouble(0, 10.0);
+
+        isheads = random.nextBoolean();
+
+        if(isheads){
+            System.out.println("HEADS");
+        }else{
+            System.out.println("TAILS");
+        }
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+
+
+        System.out.println(num4);
+    }
+}
