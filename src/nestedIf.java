85cae1c650295d71b48d6dcba89ebf184eef6ef9
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-27 14:09:08 +0800

    nested if

--- src/nestedIf.java
+++ src/nestedIf.java
@@ -3,5 +3,16 @@ public class nestedIf {
     public static void main(String[] args) {
 
         // Nested if statement
+
+        boolean isStudent = true;
+        boolean isSenior;
+        double price = 9.99;
+
+        if (isStudent) {
+            System.out.println("Student is a student");
+        }else {
+            System.out.println("Senior is a student");
+        }
+        
     }
 }
