c6f9b64e7bd25ebe827583bc4f2054e079e7984a
Emmanuel <emmanuelcabizares@gmail.com>
2026-08-28 23:13:12 +0800

    Nested if

--- src/nestedIf.java
+++ src/nestedIf.java
@@ -5,14 +5,31 @@ public class nestedIf {
         // Nested if statement
 
         boolean isStudent = true;
-        boolean isSenior;
+        boolean isSenior = true;
         double price = 9.99;
 
         if (isStudent) {
-            System.out.println("Student is a student");
+
+            if(isSenior){
+                System.out.println("You get a senior discount of 20% ");
+                System.out.println("You get a student discount of 10%");
+                price *= 0.7;
+            }else{
+                System.out.println("Student is a student");
+                price *= 0.9;
+            }
+
         }else {
-            System.out.println("Senior is a student");
+
+            if(isSenior){
+                System.out.println("You get a senior discount of 20% ");
+                price *= 0.8;
+            }else{
+                price *= 1;
+            }
         }
+
+        System.out.printf("The price of the ticket is $%.2f", price);
         
     }
 }
