fabc0d8fc304b79ee70b20f10c12ab4e63783633
Emmanuel <emmanuelcabizares@gmail.com>
2026-09-23 10:55:23 +0800

    Initialize Dice program

---
+++ src/diceProgram.java
@@ -0,0 +1,19 @@
+public class diceProgram {
+
+    public static void main(String[] args) {
+
+        // Dice Roller Program
+
+        // DECLARE VARIABLE
+
+        // GET # OF DICE FROM THE USER
+
+        // CHECK IF # DICE > 0
+
+        // ROLL ALL THE DICE
+
+        // GET TOTAL
+
+        // DISPLAY ASCII OF DICE
+    }
+}
