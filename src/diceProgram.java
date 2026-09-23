6cd70519b6b26d1531986e32dd34670a44270bef
Emmanuel <emmanuelcabizares@gmail.com>
2026-09-23 12:00:27 +0800

    Break dice

--- src/diceProgram.java
+++ src/diceProgram.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class diceProgram {
 
     public static void main(String[] args) {
@@ -5,15 +8,31 @@ public class diceProgram {
         // Dice Roller Program
 
         // DECLARE VARIABLE
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int numOfDice;
+        int total;
 
         // GET # OF DICE FROM THE USER
 
+        System.out.print("Enter the # of dice to roll: ");
+        numOfDice = scanner.nextInt();
+
         // CHECK IF # DICE > 0
+
+        if(numOfDice > 0){
+            System.out.println("You roll the dice");
+        }else{
+            System.out.println("# of dice must be greater than 0");
+        }
 
         // ROLL ALL THE DICE
 
         // GET TOTAL
 
         // DISPLAY ASCII OF DICE
+
+        scanner.close();
     }
 }
