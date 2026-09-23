1602fd196fd12027619b4fc6a2957bd21b616d9a
Emmanuel <emmanuelcabizares@gmail.com>
2026-09-23 09:17:41 +0800

    Added variable scope

---
+++ src/variableScope.java
@@ -0,0 +1,18 @@
+public class variableScope {
+
+    static int x = 3; // SCOPE
+
+    public static void main(String[] args) {
+
+        // Variable scope = where a variable can be accessed
+
+        int x = 1; // LOCAL VARIABLE
+
+        doSomething();
+    }
+    static void doSomething(){
+        int x = 2; // LOCAL VARIABLE
+
+        System.out.println(x);
+    }
+}
